e18874336c1bdadf6b1ee231c554a0a1c48c07b0
Jaehyeon <wogus392@gmail.com>
2024-05-28 21:16:24 +0900

    feat: add assets

---
+++ src/scenes/LoadingScene.js
@@ -0,0 +1,6 @@
+export default class LoadingScene extends Phaser.Scene {
+    constructor() {
+          // super에 파라미터로 넘겨주는 string이 해당 scene의 identifier가 됩니다.
+      super("loadGame");
+    }
+  }
